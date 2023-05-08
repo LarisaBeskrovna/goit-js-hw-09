b344736d81ffb3b6062b54524a62e19f1a0bc5a9
LarisaBeskrovna <117515454+LarisaBeskrovna@users.noreply.github.com>
2023-05-08 08:16:09 +0000

    Deploying to gh-pages from @ LarisaBeskrovna/goit-js-hw-09@b7fed62443f4c6a64cf5d753f18a19e64bca245d 🚀

---
+++ 01-color-switcher.73eaaa6d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o={timerId:null,isActive:!1,changeBodyColor(){this.isActive||(this.timerId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0,e.disabled=!1)},stopBodyColor(){clearInterval(this.timerId),t.disabled=!1,e.disabled=!0}};t.addEventListener("click",(()=>{o.changeBodyColor()})),e.addEventListener("click",(()=>{o.stopBodyColor()}));
+//# sourceMappingURL=01-color-switcher.73eaaa6d.js.map
