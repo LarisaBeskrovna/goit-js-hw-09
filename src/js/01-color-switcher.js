b7fed62443f4c6a64cf5d753f18a19e64bca245d
LarisaBeskrovna <117515454+LarisaBeskrovna@users.noreply.github.com>
2023-05-08 11:15:30 +0300

    task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  const startBtn = document.querySelector('button[data-start]');
+  const stopBtn = document.querySelector('button[data-stop]');
+  
+  
+  const bodyColor = {
+    timerId: null,
+    isActive: false,
+  changeBodyColor () {
+        if(this.isActive) {
+            return;
+        }
 
+  this.timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+    },
+
+    stopBodyColor () {
+        clearInterval(this.timerId);
+        startBtn.disabled = false;
+        stopBtn.disabled = true;
+    }
+  }
+  
+  startBtn.addEventListener('click', () => {bodyColor.changeBodyColor()});
+  stopBtn.addEventListener('click', () => {bodyColor.stopBodyColor()});
+  
